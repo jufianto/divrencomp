ca83feb23e23effde2e056fa15e01571f890242a
jufianto <jufiantohendri@gmail.com>
2017-05-11 18:49:55 +0700

    add module sparepart

---
+++ application/views/editSpare.php
@@ -0,0 +1,52 @@
+<div class="container-fluid">
+    <?php echo validation_errors();
+     ?>
+  <!-- Page Heading -->
+  <div class="row">
+    <div class="col-lg-12">
+      <h1 class="page-header">
+        Tambah Data Sparepart
+      </h1>
+      <ol class="breadcrumb">
+        <li>
+          <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
+        </li>
+        <li class="active">
+          <i class="fa fa-edit"></i> <a href="pelanggan.php">Data Sparepart</a>
+        </li>
+        <li class="active">
+          <i class="fa fa-edit"></i> Tambah Data Sparepart
+        </li>
+      </ol>
+    </div>
+  </div>
+  <!-- /.row -->
+  <!-- form edit data service -->
+  <div class="row">
+    <div class="col-md-6">
+
+
+      <form role="form" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+
+
+        <div class="form-group">
+          <label>Nama Barang</label>
+          <input class="form-control" value="<?= $record->nama_sparepart ?>"  required name="nama">
+        </div>
+        <div class="form-group">
+          <label>Harga</label>
+          <input class="form-control" value="<?= $record->harga_sparepart ?>" required name="harga">
+        </div>
+
+      </br>
+    </br>
+
+    <div class="form-group">
+      <Button class="btn btn-primary btn-xl page-scroll">Simpan</button>
+
+      </div>
+    </form>
+
+  </div>
+
+</div>
